56049dc934d63867449674ff75553bd28990f121
andre-fuchs-uni-oldenburg <64791323+andre-fuchs-uni-oldenburg@users.noreply.github.com>
2021-05-16 13:34:24 +0200

    Delete V4 Functions directory

--- V4 Functions/fig_setup.m
+++
@@ -1,7 +0,0 @@
-set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
-set(groot, 'defaulttextinterpreter','latex');   
-set(groot, 'defaultLegendInterpreter','latex');
-set(groot, 'defaultAxesTitle','latex');
-set(gca,'FontSize',22)
-legend1 = legend(gca,'show');
-set(legend1,'FontSize',18);
